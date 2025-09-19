c88e17b38753b3de142b609ecfa809f96d5fd49b
ACE638 <pedrogames20.com@gmail.com>
2025-09-19 20:08:53 -0300

    Simple query with query methods

--- src/main/java/com/pedro/workshopmongodb/resources/PostResource.java
+++ src/main/java/com/pedro/workshopmongodb/resources/PostResource.java
@@ -1,14 +1,12 @@
 package com.pedro.workshopmongodb.resources;
 
 import com.pedro.workshopmongodb.domain.Post;
-import com.pedro.workshopmongodb.domain.Post;
+import com.pedro.workshopmongodb.resources.util.URL;
 import com.pedro.workshopmongodb.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -22,5 +20,12 @@ public class PostResource {
     public ResponseEntity<Post> findById(@PathVariable String id) {
         Post obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @GetMapping(value = "/titlesearch")
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
     }
 }
