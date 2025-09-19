348044dbfbd272e365a8dadf0ea2e9108b5104bb
ACE638 <pedrogames20.com@gmail.com>
2025-09-19 19:33:47 -0300

    Endpoint to return a user's posts

--- src/main/java/com/pedro/workshopmongodb/resources/UserResource.java
+++ src/main/java/com/pedro/workshopmongodb/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.pedro.workshopmongodb.resources;
 
+import com.pedro.workshopmongodb.domain.Post;
 import com.pedro.workshopmongodb.domain.User;
 import com.pedro.workshopmongodb.dto.UserDTO;
 import com.pedro.workshopmongodb.service.UserService;
@@ -54,5 +55,11 @@ public class UserResource {
         obj.setId(id);
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping(value = "/{id}/posts")
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj.getPosts());
     }
 }
