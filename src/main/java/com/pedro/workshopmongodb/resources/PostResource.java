7ec8f7f2062af155e802c1c985f6cbd57c95a93b
ACE638 <pedrogames20.com@gmail.com>
2025-09-19 19:42:12 -0300

    Getting a post by id

---
+++ src/main/java/com/pedro/workshopmongodb/resources/PostResource.java
@@ -0,0 +1,26 @@
+package com.pedro.workshopmongodb.resources;
+
+import com.pedro.workshopmongodb.domain.Post;
+import com.pedro.workshopmongodb.domain.Post;
+import com.pedro.workshopmongodb.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+
+    @Autowired
+    private PostService service;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Post> findById(@PathVariable String id) {
+        Post obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+}
---
+++ src/main/java/com/pedro/workshopmongodb/service/PostService.java
@@ -0,0 +1,22 @@
+package com.pedro.workshopmongodb.service;
+
+import com.pedro.workshopmongodb.domain.Post;
+import com.pedro.workshopmongodb.repository.PostRepository;
+import com.pedro.workshopmongodb.service.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repository;
+
+    public Post findById(String id) {
+        Optional<Post> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+}
