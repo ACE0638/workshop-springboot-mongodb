5c0b84f1a423c73c6b9bc8908e48c644ddb1a5de
ACE638 <pedrogames20.com@gmail.com>
2025-09-19 17:25:37 -0300

    Getting a user by id

--- src/main/java/com/pedro/workshopmongodb/service/UserService.java
+++ src/main/java/com/pedro/workshopmongodb/service/UserService.java
@@ -2,16 +2,25 @@ package com.pedro.workshopmongodb.service;
 
 import com.pedro.workshopmongodb.domain.User;
 import com.pedro.workshopmongodb.repository.UserRepository;
+import com.pedro.workshopmongodb.service.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
+
     @Autowired
     private UserRepository repository;
+
     public List<User> findAll(){
         return repository.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 }
